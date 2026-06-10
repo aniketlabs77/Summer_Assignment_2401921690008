a8850a8a4eebe0e0eb31d03866304548e4b0ba52
aniketlabs77 <aniketsinghchauhan77@gmail.com>
2026-06-10 18:23:15 +0530

    week 2 day 3 work

---
+++ WEEK_2/Day3/Is Subsequence Solved.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size(); 
+        int m = t.size(); 
+
+        int i = 0 ; 
+        for(int j = 0 ; j<m ; j++){
+            if(s[i]==t[j]) i++ ; 
+
+            if(i==n) return true;  
+        }
+
+        if(i==n) return true;  
+        return false; 
+    }
+};
